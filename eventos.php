b8af6f1e43fb854e0cbc3246e05fd8be27a73cb8
theprap <rikymon2@hotmail.com>
2017-06-02 19:20:38 -0500

    se agrego findEventoPalabra a eventos

--- eventos.php
+++ eventos.php
@@ -387,6 +387,68 @@ function setEvento(){
 }
 
 /**
+ * Funcion encargada de buscar los eventos existentes por medio de una palabra clave
+ */
+function findEventoPalabra(){
+    // Validacion de que la url contenga el valor del id
+    if(isset($_GET['pal'])){
+
+        // Asignacion de la variable del id
+        $palabra = $_GET['pal'];
+
+        // Conexion a la base de datos
+        $con = conexion();
+
+        // Selecciona todos los eventos que contengan la palabra enviada
+        $query = "SELECT * FROM eventos e, categorias c WHERE e.cat_id = c.cat_id AND (e.eve_nombre LIKE '%".$palabra."%')";
+        $select = $con -> query($query);
+
+        // Validacion de que existan eventos con la palabra en la base de datos
+        if(($select -> num_rows) > 0){
+
+            // Creacion de array vacio que contendra todos los eventos encontrados
+            $arrayEventos = array();
+
+            // Mientras exista un evento en la busqueda crea un array del evento y lo agrega a la variable de
+            // todos los eventos que existen
+            while($row = $select->fetch_assoc()){
+                $evento = [
+                    "id" => $row['eve_id'],
+                    "nombre" => $row['eve_nombre'],
+                    "estado" => $row['eve_estado'],
+                    "ciudad" => $row['eve_ciudad'],
+                    "direccion" => $row['eve_direccion'],
+                    "lugar" => $row['eve_lugar'],
+                    "fecha" => $row['eve_fecha'],
+                    "hora" => $row['eve_hora'],
+                    "foto" => $row['eve_foto'],
+                    "descripcion" => $row['eve_descripcion'],
+                    "categoria" => $row['cat_nombre']
+                ];
+                $arrayEventos[] = $evento;
+            }
+
+            // Crea el array de respuesta con todos los eventos encontrados de la base de datos
+            $eventos = ["res" => "1", "eventos" => $arrayEventos];
+
+            // Creacion del JSON, cierre de la conexion a la base de datos e imprime el JSON
+            $json = json_encode($eventos);
+            $con -> close();
+            print($json);
+        }else{
+            // Respuesta en caso de que no haya registros del evento tipo culturales
+            $json = json_encode(["res"=>"0", "msg"=>"No hay eventos que contengan ".$palabra]);
+            $con -> close();
+            print($json);
+        }
+    }else{
+        // Respuesta en caso de que no contenga id el url
+        $json = json_encode(["res"=>"0", "msg"=>"La operación deseada no existe"]);
+        print($json);
+    }
+}
+
+/**
  * segmento encargado de verificar que la url tenga el parametro de la accion, en caso de no tener el parametro a
  * regresa un json con res=0 y un msg de operacion no existe en caso contrario conprueba la accion y dependiendo
  * de la accion solicitada realiza una funcion especifrica
@@ -421,6 +483,10 @@ if(isset($_GET['a'])){
 /*------------------------------------------------------------------------------------- OBTENCION EVENTO POR ID ------*/
         case 'setEvento':
             setEvento();
+            break;
+/*------------------------------------------------------------------------------------- OBTENCION EVENTO POR ID ------*/
+        case 'findEventoPalabra':
+            findEventoPalabra();
             break;
 /*------------------------------------------------------------------------------------- CASO DEFAULT -----------------*/
         default:
